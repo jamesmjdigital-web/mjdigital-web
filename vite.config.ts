baca90e2a7007e7caeffc35c01d171ede7dea3d2
MJ <james.mjdigital@gmail.com>
2025-12-01 17:19:27 -0500

    FIX YES

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
-export default defineConfig({
-  base: "/mjdigital-web/",
+export default defineConfig(({ mode }) => ({
+  base: mode === "production" ? "/mjdigital-web/" : "/",
   server: {
     host: "::",
     port: 8080,
@@ -14,4 +14,4 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-});
+}));
