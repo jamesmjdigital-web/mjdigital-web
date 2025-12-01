63ae227eb5c39846826efa5be3263889c9329d65
MJ <james.mjdigital@gmail.com>
2025-12-01 14:40:00 -0500

    configure Vite for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/mjdigital-web/",
   server: {
     host: "::",
     port: 8080,
@@ -16,3 +15,4 @@ export default defineConfig(({ mode }) => ({
     },
   },
 }));
+
